9f3a36acb163f4b6cc42eed4f814c6b62baa4378
Romain Fromi <romain.fromi@gmail.com>
2025-03-31 17:43:28 +0200

    Prepare scoring

--- rules/src/LesJardinsSuspendusRules.ts
+++ rules/src/LesJardinsSuspendusRules.ts
@@ -1,4 +1,16 @@
-import { HiddenMaterialRules, hideItemId, MaterialGame, MaterialItem, MaterialMove, PositiveSequenceStrategy, TimeLimit } from '@gamepark/rules-api'
+import {
+  CompetitiveScore,
+  getEnumValues,
+  HiddenMaterialRules,
+  hideItemId,
+  MaterialGame,
+  MaterialItem,
+  MaterialMove,
+  PositiveSequenceStrategy,
+  TimeLimit
+} from '@gamepark/rules-api'
+import { sumBy } from 'lodash'
+import { Flower } from './material/Garden'
 import { LocationType } from './material/LocationType'
 import { MaterialType } from './material/MaterialType'
 import { PlayerColor } from './PlayerColor'
@@ -15,7 +27,9 @@ import { RuleId } from './rules/RuleId'
  */
 export class LesJardinsSuspendusRules
   extends HiddenMaterialRules<PlayerColor, MaterialType, LocationType>
-  implements TimeLimit<MaterialGame<PlayerColor, MaterialType, LocationType>, MaterialMove<PlayerColor, MaterialType, LocationType>, PlayerColor>
+  implements
+    CompetitiveScore<MaterialGame<PlayerColor, MaterialType, LocationType>, MaterialMove<PlayerColor, MaterialType, LocationType>, PlayerColor>,
+    TimeLimit<MaterialGame<PlayerColor, MaterialType, LocationType>, MaterialMove<PlayerColor, MaterialType, LocationType>, PlayerColor>
 {
   rules = {
     [RuleId.PlaceGardenCard]: PlaceGardenCardRule,
@@ -48,6 +62,50 @@ export class LesJardinsSuspendusRules
     [MaterialType.EnhancementTile]: {
       [LocationType.EnhancementPile]: (item: MaterialItem) => (item.location.rotation ? ['id.front'] : [])
     }
+  }
+
+  getScore(player: PlayerColor): number {
+    return (
+      this.scoreIrrigation(player) +
+      sumBy(getEnumValues(Flower), (flower) => this.scoreBlooms(player, flower)) +
+      this.scoreTrees(player) +
+      this.scoreAnimals(player) +
+      this.scoreVisitors(player) +
+      this.scoreRoyalObjectives(player)
+    )
+  }
+
+  scoreIrrigation(_playerId: PlayerColor): number {
+    return 0
+  }
+
+  scoreBlooms(_playerId: PlayerColor, _flower: Flower): number {
+    return 0
+  }
+
+  scoreTrees(_playerId: PlayerColor): number {
+    return 0
+  }
+
+  scoreAnimals(_playerId: PlayerColor): number {
+    return 0
+  }
+
+  scoreVisitors(_playerId: PlayerColor): number {
+    return 0
+  }
+
+  scoreRoyalObjectives(_playerId: PlayerColor): number {
+    return 0
+  }
+
+  getTieBreaker(tieBreaker: number, player: PlayerColor) {
+    if (tieBreaker === 1) {
+      const gold = this.material(MaterialType.GoldCoin).location(LocationType.PlayerGoldCoins).player(player).getQuantity()
+      const tools = this.material(MaterialType.Tool).location(LocationType.PlayerTools).player(player).getQuantity()
+      return gold + tools
+    }
+    return
   }
 
   giveTime(): number {
