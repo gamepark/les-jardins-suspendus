388a9f6059f6eeae1a26ce5ba83d45842becdcd4
Romain Fromi <romain.fromi@gmail.com>
2025-04-15 10:44:55 +0200

    GoldCoinHelp.tsx

--- app/src/material/GoldCoinDescription.ts
+++ app/src/material/GoldCoinDescription.ts
@@ -1,10 +1,12 @@
 import { TokenDescription } from '@gamepark/react-game'
 import GoldCoin from '../images/GoldCoin.png'
+import { GoldCoinHelp } from './help/GoldCoinHelp'
 
 class GoldCoinDescription extends TokenDescription {
   width = 2.58
   height = 2.5
   image = GoldCoin
+  help = GoldCoinHelp
 }
 
 export const goldCoinDescription = new GoldCoinDescription()
---
+++ app/src/material/help/GoldCoinHelp.tsx
@@ -0,0 +1,56 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
+import { LesJardinsSuspendusRules } from '@gamepark/les-jardins-suspendus/LesJardinsSuspendusRules'
+import { EnhancementType } from '@gamepark/les-jardins-suspendus/material/Enhancement'
+import { LocationType } from '@gamepark/les-jardins-suspendus/material/LocationType'
+import { MaterialType } from '@gamepark/les-jardins-suspendus/material/MaterialType'
+import { PlayerColor } from '@gamepark/les-jardins-suspendus/PlayerColor'
+import { linkButtonCss, MaterialHelpProps, Picture, PlayMoveButton, usePlayerId, usePlayerName, useRules } from '@gamepark/react-game'
+import { MaterialMoveBuilder } from '@gamepark/rules-api'
+import { Trans, useTranslation } from 'react-i18next'
+import GoldCoin from '../../images/GoldCoin.png'
+import displayMaterialHelp = MaterialMoveBuilder.displayMaterialHelp
+
+export const GoldCoinHelp = ({ item }: MaterialHelpProps) => {
+  const { t } = useTranslation()
+  const rules = useRules<LesJardinsSuspendusRules>()!
+  const me = usePlayerId<PlayerColor>()
+  const firstEnhancementTile = rules
+    .material(MaterialType.EnhancementTile)
+    .location(LocationType.EnhancementPile)
+    .locationId(EnhancementType.Simple)
+    .maxBy((item) => item.location.x!)
+  const displayFirstEnhancementTileHelp = displayMaterialHelp(MaterialType.EnhancementTile, firstEnhancementTile.getItem(), firstEnhancementTile.getIndex())
+  const owner = item.location?.player
+  const player = usePlayerName(owner)
+  return (
+    <>
+      <h2>{t('gold')}</h2>
+      <p>
+        <Trans
+          defaults="gold.use"
+          components={{
+            enhancement: <PlayMoveButton css={linkButtonCss} move={displayFirstEnhancementTileHelp} transient />
+          }}
+        />
+      </p>
+      {owner !== undefined && (
+        <p>
+          <Trans
+            defaults={owner === me ? 'gold.you' : 'gold.player'}
+            values={{ gold: item.quantity, player }}
+            components={{
+              gold: <Picture src={GoldCoin} css={pictureCss} />
+            }}
+          />
+        </p>
+      )}
+    </>
+  )
+}
+
+const pictureCss = css`
+  display: inline-block;
+  vertical-align: sub;
+  height: 1.5em;
+`
