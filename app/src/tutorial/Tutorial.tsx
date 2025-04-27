2cc32cbee384e103887227aff5fe7af272a6a802
Romain Fromi <romain.fromi@gmail.com>
2025-04-27 22:15:07 +0200

    tuto.objective.count

--- app/src/tutorial/Tutorial.tsx
+++ app/src/tutorial/Tutorial.tsx
@@ -314,6 +314,20 @@ export class Tutorial extends MaterialTutorial {
         margin: { top: 1, bottom: 1, right: 1 },
         scale: 0.7
       })
+    },
+    {
+      popup: {
+        text: () => <Trans defaults="tuto.objective.count" components={BaseComponents} />,
+        position: { x: 20 }
+      },
+      focus: (game) => ({
+        materials: [
+          this.material(game, MaterialType.GardenCard).id(Garden.EmptyRYIrrigation),
+          this.material(game, MaterialType.EnhancementTile).id<EnhancementId>((id) => id.front === Enhancement.FlowerRR)
+        ],
+        margin: { top: 1, bottom: 1, right: 1 },
+        scale: 0.7
+      })
     }
   ]
 
