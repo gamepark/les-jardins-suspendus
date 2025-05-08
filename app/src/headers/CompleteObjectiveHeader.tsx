af21633a872f66e009257a6e24953fcc3c74d1bb
Romain Fromi <romain.fromi@gmail.com>
2025-05-08 14:03:27 +0200

    Auto validate when I cannot complete an objective

--- app/src/headers/CompleteObjectiveHeader.tsx
+++ app/src/headers/CompleteObjectiveHeader.tsx
@@ -2,7 +2,7 @@
 import { LesJardinsSuspendusRules } from '@gamepark/les-jardins-suspendus/LesJardinsSuspendusRules'
 import { PlayerColor } from '@gamepark/les-jardins-suspendus/PlayerColor'
 import { CustomMoveType } from '@gamepark/les-jardins-suspendus/rules/CustomMoveType'
-import { PlayMoveButton, useLegalMove, usePlayerId, usePlayerName, useRules } from '@gamepark/react-game'
+import { PlayMoveButton, useLegalMoves, usePlayerId, usePlayerName, useRules } from '@gamepark/react-game'
 import { isCustomMoveType } from '@gamepark/rules-api'
 import { Trans, useTranslation } from 'react-i18next'
 
@@ -12,16 +12,28 @@ export const CompleteObjectiveHeader = () => {
   const me = usePlayerId<PlayerColor>()
   const activePlayer = rules.getActivePlayer()
   const player = usePlayerName(activePlayer)
-  const pass = useLegalMove(isCustomMoveType(CustomMoveType.Pass))
+  const legalMoves = useLegalMoves()
+  const pass = legalMoves.find(isCustomMoveType(CustomMoveType.Pass))
   if (activePlayer === me) {
-    return (
-      <Trans
-        defaults="header.objective.you"
-        components={{
-          pass: <PlayMoveButton move={pass} />
-        }}
-      />
-    )
+    if (legalMoves.length > 1) {
+      return (
+        <Trans
+          defaults="header.objective.you"
+          components={{
+            pass: <PlayMoveButton move={pass} />
+          }}
+        />
+      )
+    } else {
+      return (
+        <Trans
+          defaults="header.objective.pass"
+          components={{
+            pass: <PlayMoveButton move={pass} auto={10} />
+          }}
+        />
+      )
+    }
   } else {
     return <>{t('header.objective.player', { player })}</>
   }
